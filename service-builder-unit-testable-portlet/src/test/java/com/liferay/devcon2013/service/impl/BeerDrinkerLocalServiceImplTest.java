8d302805033e8e51b5abd22442a8e3c6d508d8db
Thiago Leão Moreira <tmoreira2020@gmail.com>
2013-10-02 15:17:09 -0300

    @add:;Added method to test if content of name parameter is null.

--- service-builder-unit-testable-portlet/src/test/java/com/liferay/devcon2013/service/impl/BeerDrinkerLocalServiceImplTest.java
+++ service-builder-unit-testable-portlet/src/test/java/com/liferay/devcon2013/service/impl/BeerDrinkerLocalServiceImplTest.java
@@ -1,5 +1,25 @@
 package com.liferay.devcon2013.service.impl;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.liferay.portal.service.ServiceContext;
+
 public class BeerDrinkerLocalServiceImplTest {
 
+	@Test
+	public void testAddBeerDrinkerWithNullName() {
+		String name = null;
+		float alcoholLevel = 0;
+		ServiceContext serviceContext = new ServiceContext();
+		BeerDrinkerLocalServiceImpl impl = new BeerDrinkerLocalServiceImpl();
+
+		try {
+			impl.addBeerDrinker(name, alcoholLevel, serviceContext);
+			fail("It must throw an " + IllegalArgumentException.class.getName());
+		} catch (Exception e) {
+			assertEquals(IllegalArgumentException.class, e.getClass());
+		}
+	}
 }
