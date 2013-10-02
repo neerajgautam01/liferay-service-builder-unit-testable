b9539385ec165769b24cd2473570b8f6c5cc5c00
Thiago Leão Moreira <tmoreira2020@gmail.com>
2013-10-02 15:20:09 -0300

    @add:;Added method to test if content of name parameter is empty.

--- service-builder-unit-testable-portlet/src/test/java/com/liferay/devcon2013/service/impl/BeerDrinkerLocalServiceImplTest.java
+++ service-builder-unit-testable-portlet/src/test/java/com/liferay/devcon2013/service/impl/BeerDrinkerLocalServiceImplTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.service.ServiceContext;
 
 public class BeerDrinkerLocalServiceImplTest {
@@ -11,6 +12,21 @@ public class BeerDrinkerLocalServiceImplTest {
 	@Test
 	public void testAddBeerDrinkerWithNullName() {
 		String name = null;
+		float alcoholLevel = 0;
+		ServiceContext serviceContext = new ServiceContext();
+		BeerDrinkerLocalServiceImpl impl = new BeerDrinkerLocalServiceImpl();
+
+		try {
+			impl.addBeerDrinker(name, alcoholLevel, serviceContext);
+			fail("It must throw an " + IllegalArgumentException.class.getName());
+		} catch (Exception e) {
+			assertEquals(IllegalArgumentException.class, e.getClass());
+		}
+	}
+
+	@Test
+	public void testAddBeerDrinkerWithEmptyName() {
+		String name = StringPool.BLANK;
 		float alcoholLevel = 0;
 		ServiceContext serviceContext = new ServiceContext();
 		BeerDrinkerLocalServiceImpl impl = new BeerDrinkerLocalServiceImpl();
